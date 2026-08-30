1f9be44a36e57c2a8258f1645f4db94f403c945a
Vaielet <zunzviolette@gmail.com>
2026-08-30 09:02:04 +0200

    Add files via upload

--- pages/2_🛒_Générer_ma_liste.py
+++ pages/2_🛒_Générer_ma_liste.py
@@ -163,8 +163,7 @@ for i, name in enumerate(names):
 
     with col:
         with st.container(border=True):
-            image = common.get_recipe_image(name, recipe["image"])
-            common.render_recipe_image_card(name, image)
+            common.render_recipe_image_card(name, recipe["image"])
 
             if recipe.get("tags"):
                 st.markdown(" ".join(f"`{t}`" for t in recipe["tags"]))
