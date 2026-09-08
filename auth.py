9812b457eb4343586249fd736cf6af307e209ea7
Vaielet <zunzviolette@gmail.com>
2026-09-08 15:10:32 +0200

    Update auth.py

--- auth.py
+++ auth.py
@@ -106,8 +106,9 @@ def render_sidebar_auth() -> None:
 
             with signup_tab:
                 st.caption(
-                    "Tu recevras directement le statut éditeur·rice, ce qui "
-                    "te permettra d'ajouter des recettes."
+                    "Créer un compte "
+                    "te permettra d'ajouter des recettes et de sauvegarder les menus "
+                    "que tu as composés."
                 )
                 with st.form("_auth_signup_form", clear_on_submit=True):
                     new_username = st.text_input("Choisis un identifiant")
