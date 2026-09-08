cd274c85b95c1a149ee4fa54ecab6801cc91dbd7
Vaielet <zunzviolette@gmail.com>
2026-09-08 17:13:03 +0200

    Update Accueil.py

--- Accueil.py
+++ Accueil.py
@@ -146,6 +146,20 @@ if auth.can_manage_products():
 # (pg.run() plus bas) et qui gère les URLs.
 pg = st.navigation(pages, position="hidden")
 
+st.markdown(
+    <style>
+      div[data-testid="stSidebarHeader"] > img, div[data-testid="collapsedControl"] > img {
+      height: 3rem;
+      width: auto;
+      }
+  
+      div[data-testid="stSidebarHeader"], div[data-testid="stSidebarHeader"] > *,
+      div[data-testid="collapsedControl"], div[data-testid="collapsedControl"] > * {
+      display: flex;
+      align-items: center;
+      }
+    </style>
+)
 st.logo(
     image="images/CookPotes_logo.png",
     #icon_image="chemin/vers/votre_icone.png", # Facultatif : affiché quand la sidebar est réduite
