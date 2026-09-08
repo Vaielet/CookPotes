a0d15b80e47db1d7d20cbb343f3da926b313c81f
Vaielet <zunzviolette@gmail.com>
2026-09-08 15:19:01 +0200

    Update Accueil.py

--- Accueil.py
+++ Accueil.py
@@ -53,7 +53,7 @@ Bienvenue ! Cette application te permet de :
         """
     )
 
-    col1, col2 = st.columns(2,gap="large")
+    col1, col2 = st.columns(2,gap="large",border=True)
     with col1:
         if common.icon_button("Composer mon menu", "generer_mon_menu.png", "🛒", key="home-btn-generer"):
             st.switch_page("pages/2_Composer_mon_menu.py")
