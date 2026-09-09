2761150daf2f6761d40544eac15f03b8ac2d3388
Vaielet <zunzviolette@gmail.com>
2026-09-09 23:50:30 +0200

    Update Accueil.py

--- Accueil.py
+++ Accueil.py
@@ -84,23 +84,12 @@ Il est temps de poser les armes. CookPotes, c'est l'application qui vient sauver
         unsafe_allow_html=True,
     )
 
-    # Injection CSS pour centrer les boutons Streamlit dans leurs conteneurs
-    st.markdown(
-        """
-        <style>
-        div.stButton > button {
-            display: block;
-            margin-0 auto;
-        }
-        </style>
-        """,
-        unsafe_allow_html=True,
-    )
-
     col1, col2 = st.columns(2,gap="medium",border=False)
     with col1:
+        st.markdown('<div style="text-align: center;">', unsafe_allow_html=True)
         if common.icon_button("Je veux savoir comment ça marche", "comment_ca_marche.png", "❓", key="home-btn-comment"):
             st.switch_page("pages/Comment_ca_marche.py")
+        st.markdown("</div>", unsafe_allow_html=True)
     with col2:
         if common.icon_button("C'est parti, je veux composer mon menu", "generer_mon_menu.png", "📋", key="home-btn-generer"):
             st.switch_page("pages/2_Composer_mon_menu.py")
