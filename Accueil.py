6f0c497eae1ed3068becb8a10aa74fc03fd7aa3f
Vaielet <zunzviolette@gmail.com>
2026-09-09 23:39:56 +0200

    Update Accueil.py

--- Accueil.py
+++ Accueil.py
@@ -88,7 +88,8 @@ Il est temps de poser les armes. CookPotes, c'est l'application qui vient sauver
     with col1:
         if common.icon_button("Je veux savoir comment ça marche", "comment_ca_marche.png", "❓", key="home-btn-comment"):
             st.switch_page("pages/Comment_ca_marche.py")
-        if common.icon_button("Je veux composer mon menu", "generer_mon_menu.png", "📋", key="home-btn-generer"):
+    with col2:
+        if common.icon_button("C'est parti, je veux composer mon menu", "generer_mon_menu.png", "📋", key="home-btn-generer"):
             st.switch_page("pages/2_Composer_mon_menu.py")
         
     recipes = db.get_all_recipes()
