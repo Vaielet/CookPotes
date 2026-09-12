05b30fdbb7ab32ca4070d0a917c3333071e9b668
Vaielet <zunzviolette@gmail.com>
2026-09-12 11:59:28 +0200

    Update 1_Ajouter_une_recette.py

--- pages/1_Ajouter_une_recette.py
+++ pages/1_Ajouter_une_recette.py
@@ -37,10 +37,8 @@ st.caption(
     "de données et disponibles pour tou·tes les utilisateur·rices"
     " immédiatement sur la page « Composer mon menu »."
 )
-st.caption(
-    "Rappelle-toi, tu es le contrôle qualité : ne partage "
-    "que des recettes que tu as testées et approuvées."
-)
+st.badge("Rappelle-toi, tu es le contrôle qualité : ne partage "
+    "que des recettes que tu as testées et approuvées.", color="green")
 
 
 # ---------------------------------------------------------------------------
